d850c6ed4b207a8f70ec2d89a2d121a60cb388a9
thandayamsiva78 <tsiva7878@gmail.com>
2024-11-29 10:43:45 +0530

    grid-col-2

--- src/components/AlbumList.jsx
+++ src/components/AlbumList.jsx
@@ -30,7 +30,7 @@ const AlbumList = () => {
   };
 
   return (
-    <div className="grid grid-cols-3 md:grid-cols-3  sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-4">
+    <div className="grid grid-cols-2 md:grid-cols-3  sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-4">
       {albums.map((album) => (
         <div
           key={album.id}
